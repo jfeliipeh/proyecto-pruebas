4bfd237184ba99783ec0bac2f45620c9c780e667
jfeliipeh <100744711+jfeliipeh@users.noreply.github.com>
2025-05-22 11:24:32 -0500

    Update test_login_correcto.py

--- tests/test_login_correcto.py
+++ tests/test_login_correcto.py
@@ -3,7 +3,4 @@ def test_login_correcto():
     clave = "123"
     assert login(usuario, clave) == "Login exitoso"
 
-def login(usuario, clave):
-    if usuario == "admin" and clave == "123":
-        return "Login exitoso"
-    return "Error"
+
